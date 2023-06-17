4cd59f92bc9770d7a0a84c175fd7500191dcf7ff
Victor <victor.gomez_de_juan@contractors.roche.com>
2023-06-17 18:53:20 +0200

    When reading several barcodes the barcode interpreter render the correspondign views

--- PointOfSaleTests/BarcodeInterpreterCallsToViewRendererTests.cs
+++ PointOfSaleTests/BarcodeInterpreterCallsToViewRendererTests.cs
@@ -19,4 +19,31 @@ public class BarcodeInterpreterCallsToViewRendererTests {
 
         viewRenderer.Verify(v => v.Render(view), Times.Once);
     }
+
+    [Fact]
+    public void ReadSeveralBarcodes() {
+        var textReader = new StringReader("11111\r\n22222\r\n33333\r\n");
+        var barcodeListener = new Mock<IBarcodeListener>();
+        var viewRenderer = new Mock<IViewRenderer>();
+        SaleOneItemView view1 = new("viewName1", new Dictionary<string, object>() { { "key1", "value1" } });
+        SaleOneItemView view2 = new("viewName2", new Dictionary<string, object>() { { "key2", "value2" } });
+        SaleOneItemView view3 = new("viewName3", new Dictionary<string, object>() { { "key3", "value3" } });
+        barcodeListener
+            .Setup<SaleOneItemView>(b => b.OnBarcode("11111"))
+            .Returns(view1);
+        barcodeListener
+            .Setup<SaleOneItemView>(b => b.OnBarcode("22222"))
+            .Returns(view2);
+        barcodeListener
+            .Setup<SaleOneItemView>(b => b.OnBarcode("33333"))
+            .Returns(view3);
+
+        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
+
+        interpreter.Process();
+
+        viewRenderer.Verify(v => v.Render(view1), Times.Once);
+        viewRenderer.Verify(v => v.Render(view2), Times.Once);
+        viewRenderer.Verify(v => v.Render(view3), Times.Once);
+    }
 }
