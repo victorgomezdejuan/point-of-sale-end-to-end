b78047892f37f83d31de179e17ddf352d87c8b70
Victor <victor.gomez_de_juan@contractors.roche.com>
2023-06-17 19:43:12 +0200

    Improve test readability

--- PointOfSaleTests/BarcoreInterpreterCallsToBarcodeListenerTests.cs
+++ PointOfSaleTests/BarcoreInterpreterCallsToBarcodeListenerTests.cs
@@ -4,27 +4,37 @@ using PointOfSale.Interfaces;
 
 namespace PointOfSaleTests;
 public class BarcoreInterpreterCallsToBarcodeListenerTests {
+    private readonly Mock<IBarcodeListener> barcodeListener;
+    private readonly Mock<IViewRenderer> viewRenderer;
+
+    public BarcoreInterpreterCallsToBarcodeListenerTests() {
+        barcodeListener = new Mock<IBarcodeListener>();
+        viewRenderer = new Mock<IViewRenderer>();
+    }
+
     [Fact]
     public void ReadABarcode() {
+        // Arrange
         TextReader textReader = new StringReader("12345\r\n");
-        var barcodeListener = new Mock<IBarcodeListener>();
-        var viewRenderer = new Mock<IViewRenderer>();
-        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
 
+        // Act
+        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
         interpreter.Process();
 
+        // Assert
         barcodeListener.Verify(b => b.OnBarcode("12345"), Times.Once);
     }
 
     [Fact]
     public void ReadSeveralBarcodes() {
+        // Arrange
         var textReader = new StringReader("12345\r\n11111\r\n22222\r\n");
-        var barcodeListener = new Mock<IBarcodeListener>();
-        var viewRenderer = new Mock<IViewRenderer>();
-        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
 
+        // Act
+        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
         interpreter.Process();
 
+        // Assert
         barcodeListener.Verify(b => b.OnBarcode("12345"), Times.Once);
         barcodeListener.Verify(b => b.OnBarcode("11111"), Times.Once);
         barcodeListener.Verify(b => b.OnBarcode("22222"), Times.Once);
@@ -32,13 +42,14 @@ public class BarcoreInterpreterCallsToBarcodeListenerTests {
 
     [Fact]
     public void ReadEmptyBarcode() {
+        // Arrange
         var textReader = new StringReader("\r\n");
-        var barcodeListener = new Mock<IBarcodeListener>();
-        var viewRenderer = new Mock<IViewRenderer>();
-        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
 
+        // Act
+        BarcoreInterpreter interpreter = new(textReader, barcodeListener.Object, viewRenderer.Object);
         interpreter.Process();
 
+        // Assert
         barcodeListener.Verify(b => b.OnBarcode(""), Times.Once);
     }
 }
