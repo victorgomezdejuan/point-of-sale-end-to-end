bd2751245303927faafad05731bfc2a9dab777c4
Victor <victor.gomez_de_juan@contractors.roche.com>
2023-06-17 18:59:49 +0200

    Remove display calls from SaleOneItemHandler

--- PointOfSale/SaleOneItemHandler.cs
+++ PointOfSale/SaleOneItemHandler.cs
@@ -4,28 +4,23 @@ using PointOfSale.ValueObjects;
 namespace PointOfSale;
 public class SaleOneItemHandler : IBarcodeListener {
     private readonly ICatalog catalog;
-    private readonly IDisplay display;
 
-    public SaleOneItemHandler(ICatalog catalog, IDisplay display) {
+    public SaleOneItemHandler(ICatalog catalog) {
         this.catalog = catalog;
-        this.display = display;
     }
 
     public SaleOneItemView OnBarcode(string barcode) {
         if (barcode == "") {
-            display.DisplayEmptyCode();
             return new SaleOneItemView("Empty barcode", new Dictionary<string, object>());
         }
         else {
             Product? product = catalog.FindProductByCode(barcode);
             if (product is null) {
-                display.DisplayProductNotFound(barcode);
                 return new SaleOneItemView("Item not found", new Dictionary<string, object>() {
                     { "barcode", barcode }
                 });
             }
             else {
-                display.DisplayPrice(product.Price);
                 return new SaleOneItemView("Item found", new Dictionary<string, object>() {
                     { "price", product.Price }
                 });
--- PointOfSaleTests/SaleOneItemHandlerTests.cs
+++ PointOfSaleTests/SaleOneItemHandlerTests.cs
@@ -15,16 +15,13 @@ public class SaleOneItemHandlerTests {
         var catalogMock = new Mock<ICatalog>();
         catalogMock.Setup(c => c.FindProductByCode(productToBeFound.Code)).Returns(productToBeFound);
 
-        var displayMock = new Mock<IDisplay>();
-
-        var saleHandler = new SaleOneItemHandler(catalogMock.Object, displayMock.Object);
+        var saleHandler = new SaleOneItemHandler(catalogMock.Object);
 
         // Act
         SaleOneItemView view = saleHandler.OnBarcode(productToBeFound.Code);
 
         // Assert
         catalogMock.Verify(d => d.FindProductByCode(productToBeFound.Code));
-        displayMock.Verify(d => d.DisplayPrice(productToBeFound.Price));
         view.Name.Should().Be("Item found");
         view.Model.Should().BeEquivalentTo(
             new Dictionary<string, object>(
@@ -43,16 +40,13 @@ public class SaleOneItemHandlerTests {
         var catalogMock = new Mock<ICatalog>();
         catalogMock.Setup(c => c.FindProductByCode(productToBeFound.Code)).Returns((Product)null);
 
-        var displayMock = new Mock<IDisplay>();
-
-        var saleHandler = new SaleOneItemHandler(catalogMock.Object, displayMock.Object);
+        var saleHandler = new SaleOneItemHandler(catalogMock.Object);
 
         // Act
         SaleOneItemView view = saleHandler.OnBarcode(productToBeFound.Code);
 
         // Assert
         catalogMock.Verify(d => d.FindProductByCode(productToBeFound.Code));
-        displayMock.Verify(d => d.DisplayProductNotFound(productToBeFound.Code));
         view.Name.Should().Be("Item not found");
         view.Model.Should().BeEquivalentTo(
             new Dictionary<string, object>(
@@ -67,18 +61,14 @@ public class SaleOneItemHandlerTests {
     public void EmptyBarcode() {
         // Arrange
         var catalogMock = new Mock<ICatalog>();
-        var displayMock = new Mock<IDisplay>();
 
-        var saleHandler = new SaleOneItemHandler(catalogMock.Object, displayMock.Object);
+        var saleHandler = new SaleOneItemHandler(catalogMock.Object);
 
         // Act
         SaleOneItemView view = saleHandler.OnBarcode("");
 
         // Assert
         catalogMock.Verify(d => d.FindProductByCode(""), Times.Never);
-        displayMock.Verify(d => d.DisplayProductNotFound(""), Times.Never);
-        displayMock.Verify(d => d.DisplayPrice(It.IsAny<Price>()), Times.Never);
-        displayMock.Verify(d => d.DisplayEmptyCode());
         view.Name.Should().Be("Empty barcode");
         view.Model.Should().BeEquivalentTo(
             new Dictionary<string, object>()
